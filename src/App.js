5779f490986e2cc3f9aeb152b26c513862563b7c
sampdubs <sammypw1@gmail.com>
2021-07-10 14:47:04 -0400

    got basic calendar stuff down

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import { schedule } from "./schedule.js";
+import moment from "moment-timezone";
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+function processSchedule(schedule, utcOffset) {
+    const processed = [];
+    for (let event of schedule) {
+        const start = moment.unix(event[0] + utcOffset * 60).toDate();
+        const end = moment.unix(event[1] + utcOffset * 60).toDate();
+        const title = event[2];
+        processed.push(
+            {
+                title,
+                start,
+                end,
+            }
+        );
+    }
+
+    return processed;
+}
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const events = processSchedule(schedule, 60);
+    const localizer = momentLocalizer(moment);
+    return (
+        <div className="App">
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                defaultView="week"
+                date={moment.unix(1625976000).toDate()} // Unix timestamp for June 11
+            />
+        </div>
+    );
 }
 
 export default App;
