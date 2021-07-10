f1022ee883330d9f9dd2c2191c4a91cf11571e76
sampdubs <sammypw1@gmail.com>
2021-07-10 16:09:38 -0400

    added timezone adjustments from user location input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { schedule } from "./schedule.js";
 import moment from "moment-timezone";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useState } from 'react';
+import Geocode from "react-geocode";
+import { useEffect } from 'react';
+
+Geocode.setApiKey("AIzaSyAb94zapDceVmspV65wFj_-iV2AWQk9kwY");
 
 function processSchedule(schedule, utcOffset) {
     const processed = [];
@@ -23,10 +28,56 @@ function processSchedule(schedule, utcOffset) {
 }
 
 function App() {
-    const events = processSchedule(schedule, 60);
+    function handleLocation() {
+        Geocode.fromAddress(location)
+            .then(
+                (response) => {
+                    const { lat, lng } = response.results[0].geometry.location;
+                    console.log(lat, lng);
+                    const url = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + lat + ',' + lng + '&timestamp=1625976000&key=AIzaSyAb94zapDceVmspV65wFj_-iV2AWQk9kwY';
+                    fetch(url)
+                        .then((response) => {
+                            if (response.ok) {
+                                response.json()
+                                    .then((json) => {
+                                        setOffset((json.dstOffset + json.rawOffset) / 60);
+                                        console.log(offset);
+                                    })
+                                    .catch(() => alert("Something went wrong. Please check the location and try again."));
+                            } else {
+                                alert("Something went wrong. Please check the location and try again.");
+                            }
+                        })
+                        .catch(() => alert("Something went wrong. Please check the location and try again."));
+                },
+                (error) => {
+                    console.error(error);
+                }
+            )
+            .catch(() => alert("Something went wrong. Please check the location and try again."));
+    }
+
     const localizer = momentLocalizer(moment);
+    const [location, setLocation] = useState("Please enter your location:");
+    const [offset, setOffset] = useState(0);
+    const [events, setEvents] = useState(processSchedule(schedule, offset));
+
+    useEffect(() => {
+        console.log(offset);
+        setEvents(processSchedule(schedule, offset));
+    }, [offset]);
     return (
         <div className="App">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleLocation();
+            }}>
+                <input value={location} onChange={e => setLocation(e.target.value)} />
+                <br></br>
+                <button type="submit">Set Location!</button>
+            </form>
+            <br></br>
             <Calendar
                 localizer={localizer}
                 events={events}
